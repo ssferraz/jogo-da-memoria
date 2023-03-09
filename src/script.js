6e8bff09398bd262b8b29c88f707d6bc83026377
Samuel Ferraz <ssf@discente.ifpe.edu.br>
2023-03-09 20:31:30 -0300

    update index and js

--- src/script.js
+++ src/script.js
@@ -37,3 +37,63 @@ function unflipCards() {
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard));
+
+var matriz = [
+  ['1a', '1b', '1c', '1d'],
+  ['2a', '2b', '2c', '2d'],
+  ['3a', '3b', '3c', '3d'],
+  ['4a', '4b', '4c', '4d']
+];
+
+var icones = ['../assets/images/abacaxi.png', '../assets/images/abacaxi.png', 
+              '../assets/images/banana.png', '../assets/images/banana.png', 
+              '../assets/images/coco.png', '../assets/images/coco.png', 
+              '../assets/images/laranja.png', '../assets/images/laranja.png',  
+              '../assets/images/limao.png', '../assets/images/limao.png', 
+              '../assets/images/melancia.png', '../assets/images/melancia.png', 
+              '../assets/images/morango.png', '../assets/images/morango.png', 
+              '../assets/images/uva.png', '../assets/images/uva.png'];
+
+var button_start = document.getElementById('start-button');
+button_start.addEventListener('click', function (e) {
+  setImagens();
+})
+
+function shuffleArray(arr) {
+  // Loop em todos os elementos
+  for (let i = arr.length - 1; i > 0; i--) {
+      // Escolhendo elemento aleatório
+      const j = Math.floor(Math.random() * (i + 1));
+      // Reposicionando elemento
+      [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
+  // Retornando array com aleatoriedade
+  return arr;
+}
+
+function setImagens() {
+  //<img src="../assets/images/card.png">
+
+  var icones = ['../assets/images/abacaxi.png', '../assets/images/abacaxi.png', 
+              '../assets/images/banana.png', '../assets/images/banana.png', 
+              '../assets/images/coco.png', '../assets/images/coco.png', 
+              '../assets/images/laranja.png', '../assets/images/laranja.png',  
+              '../assets/images/limao.png', '../assets/images/limao.png', 
+              '../assets/images/melancia.png', '../assets/images/melancia.png', 
+              '../assets/images/morango.png', '../assets/images/morango.png', 
+              '../assets/images/uva.png', '../assets/images/uva.png'];
+
+  icones = shuffleArray(icones);
+  for (var linha = 0; linha < 4; linha++) {
+      for (var coluna = 0; coluna < 4; coluna++) {
+          var ref = document.getElementById(matriz[linha][coluna]);
+
+          ref.innerHTML = `<img src=${icones.pop()}></img>`;
+      }
+  }
+
+}
+
+function capturaClick() {
+
+}
